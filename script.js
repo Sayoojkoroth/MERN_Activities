3807b81722bca2adaee2a8c640119fc158fbc53c
Sayoojkoroth <sayoojkoroths79@gmail.com>
2023-11-21 20:54:23 +0530

    UI done

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("my name is Sayooj K")
+var age= prompt("Enter age : ")
+let x;
+if(age>60){
+    console.log("senior citizen")
+}
+else if(age<18){
+    console.log("child")
+}
+else{
+    console.log("youth")
+    x="youth"
+}
+
+if(x=="youth"){
+    confirm("sure you want to proceed?  ")
+}
