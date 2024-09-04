f697de1e4641d8e5e43bcefd025928520fc8b169
Sayoojkoroth <sayoojkoroths79@gmail.com>
2024-09-04 23:40:46 +0530

    commjs0409

---
+++ code.js
@@ -0,0 +1,333 @@
+// // // //console.log(Hello World!!!)
+// // // //console.log("7","8")
+// // // // let myName =" _Sayooj"; myName ="__Sk";
+// // // //  myName ="__Sku";
+// // // // console.log(myName);
+
+// // // // const pi=3.14
+// // // // let area =0, r=10;
+// // // // area = pi*r*r;
+// // // // console.log(area);
+
+// // // // let firstName =" Sayooj Koroth";
+// // // // console.log(firstName.length);
+// // // // let Name = firstName.trim();
+// // // // console.log(Name)
+// // // // console.log(Name.length);
+
+// // // // //console.log(typeof(Name));
+
+// // // // let x=2, lName="Koroth";
+// // // // console.log(typeof( + lName));
+
+// // // // let fname="sayooj", lname ="koroth"
+
+// // // // fullName = fname.concat(" " +lname);
+// // // // console.log(fullName);
+
+// // // // let num1="56", num_2 = "04";
+// // // // console.log(Number(num1)+ " " +Number(num_2));
+
+// // // let myName ="Sayooj"
+// // // let age = 24
+// // // let bio = `My name is ${myName} and my age is ${age}`;
+// // // console.log(bio);
+
+// // // let isVerified
+// // // let ageOfP1="42", ageOfP2=42;
+// // // isVerified = (ageOfP1!==ageOfP2);
+// // // console.log(`This is ${isVerified}`);
+
+// // // let myAge = 120;
+// // // if(myAge>18){
+// // //     console.log(`your age is ${myAge} which means that you are an Adult`);
+// // // }
+// // // else if(myAge==18){
+// // //     console.log("OK");
+// // // }
+
+// // // else{
+// // //     console.log("not OK");
+// // // }
+
+
+// // // let ageOC =10, drinkable;
+// // // if(ageOC<=10){
+// // //     drinkable="milk";
+// // // }
+// // // else if(ageOC>10){
+// // //     drinkable='mirinda'
+// // // }
+// // // console.log(`since your age is ${ageOC} you can only drink - ${drinkable}`);
+
+// // // drinkable = ageOC<=10 ? "MILK" : "PEPSI"
+// // // console.log(drinkable);
+
+// // // let fname ="sayooj", lnam="koroth", age1=24;
+// // // if((fname+lnam).length==14 || (age>18) ){
+// // //     console.log("application considered!!!");
+// // // }
+// // // else{
+// // //     console.log("recheck application!!!");
+// // // }
+
+
+// // // var yob = 1996;
+
+// // // switch(yob){
+// // //     case 1996: console.log("ok2");
+// // //     case 196: console.log("OK")
+// // //     case 1999 :console.log("OK1");
+// // //     default :console.log("NOT OK")   
+
+// // // }
+
+// // // // let inputFromUser = +prompt("Enter input"), sum=0, i=1;
+// // // // console.log(typeof(inputFromUser));
+// // // // let fact=1;
+
+// // // // while(i<=inputFromUser){
+// // // //     console.log(i**2);
+// // // //     i=i+1;
+// // // // }
+
+// // // // for(let x=1; x<=inputFromUser; x++){
+    
+// // // //     if(x==5){
+// // // //         console.log("skipped");
+// // // //         break;
+// // // //     }
+// // // //     console.log(x);
+// // // // }
+
+// // // let iter=10;
+
+// // // // do{
+// // // //     console.log("iter");
+// // // //     ++iter
+// // // // }
+// // // // while(iter<inputFromUser)
+
+// // // // let myFirstArray =['qwqw',5,true,"tyty"], historyArr;
+// // // // console.log(myFirstArray[2]);
+// // // // myFirstArray.unshift("newVal",7,"35345,",`"imp`);
+// // // // console.log(myFirstArray);
+// // // // let arr2= [...myFirstArray];
+// // // // console.log(arr2);
+// // // // console.log(myFirstArray[1]===arr2[1]);
+
+// // // // console.log("--------------------")
+
+// // // // const carBrands = [], userInputrecords =[];
+// // // // let userInputlimit = +prompt("Enter the number of records you want to insert :")
+// // // // for(let i=0;i<userInputlimit;i++){
+// // // //     let userrec = prompt("enter record :")
+// // // //     userInputrecords.push(userrec);
+// // // // }
+
+// // // // console.log(userInputrecords);
+
+// // // const arr1=["sayooj" ,"koroth","Male",24, '03-07-1996'];
+
+// // // // let i=0;
+// // // // while(i<=arr1.length-1){
+// // // //     console.log(` + ${arr1[i]} + `);
+// // // //     let str1= arr1[i];
+// // // //     let j=0;
+// // // //     while(j<=str1.length-1){
+// // // //         console.log(str1[j]);
+// // // //         j++
+// // // //     }
+// // // //     i++;
+// // // // }
+
+// // // // let numbers=[2,4,6,8,10,12,14,16,18,20];
+
+// // // // for(let number in numbers){
+// // // //     console.log(numbers[number]);
+// // // // }
+
+// // // // console.log("---------------")
+
+// // // // let [roll_1, roll_2, ...roll3]=numbers;
+
+// // // // const names =["sayooj","sa","gh","dd","ded","ghi","opo","paul","gtd","tdi"];
+// // // // let [name1, name2, ...name3] = names;
+// // // // console.log(Array.isArray(name3))
+
+// // // const car_obj = { Brand :"Maruti",
+// // //                   model :"800",
+// // //                   cubicCap : 799,
+// // //                   YOM : 1998,
+// // //                   ownership : "3rd" 
+// // //  }
+// // // car_obj.model='Alto';
+// // // car_obj.price=300000;
+
+// // // // let keyval="discountPrice"
+// // // //  console.log(car_obj["YOM"]);
+// // // //  car_obj[keyval]=280000;
+// // // //  console.log("----------");
+
+// // // //  const personObj ={ fname:"sayooj",
+// // // //                     lname:"koroth",
+// // // //                     age:"2#",
+// // // //                     place:"ko#######",
+// // // //                     mstatus :"Single"
+// // // //   }
+
+// // // //   personObj.gender="Male";
+// // // //   personObj["zip"]=760098;
+  
+// // // //   console.log(personObj);
+
+// // // //   for(items in personObj){
+// // // //     if(personObj[items]=="2#"){
+// // // //         personObj[items]=24;
+// // // //     }
+// // // //   }
+
+// // // //   const key1="objkey1";
+// // // //   const key2="objkey2";
+// // // //   const value1="myvalue1";
+// // // //   const value2="myvalue2";
+
+// // // //   const realobj ={};
+// // // //   realobj[key1]= value1;
+// // // //   realobj[key2]=value2;
+// // // //   console.log(realobj);
+
+// // // const newObj = {...car_obj};
+// // // console.log(newObj);
+
+// // // let [brandname,modelname] =newObj;
+// // // console.log(brandname, modelname);
+
+
+// // const arr1 =["name","phone","age",56,67000,411];
+
+// // let [var1,var2,...var3]=arr1;
+// // console.log(var1,var2, var3);
+
+// // const objcar ={car:"honda civic",
+// //                 bike:"himalayan 411",
+// //                 laptop: "asus",
+// //                 phone: "iphone",
+// //                 tablet: "samsung",
+// //                 chair:"savya",
+// //                 carNo: 56_5675
+// // }
+
+// // const newcar ={...objcar, k1:"val1"}
+// // console.log(newcar);
+
+// // let {car:mc, bike:mb, ...rest}=objcar;
+// // console.log(`my car is ${mc} and bike is ${mb}.` , rest);
+
+// // objcar.newField=null;
+// // objcar["newField1"]= undefined;
+
+// // console.log(objcar);
+
+// // for(let item in objcar){
+// //     console.log(item +" : "+objcar[item]);
+// // }
+
+// // const students=[
+// //     {name:"sk", gender:"Male", rollno:22, age:16},
+// //     {name: "rk", gender:"male", rollno:43, age:17},
+// //     {name:"pp", gender:"Female", rollno:41,age:19},
+// // ]
+
+
+// // const [{name:Name},,{gender}] = students
+// // // console.log(Name, gender);
+
+// const ages =[10,42,15,22,11,74,39,2], results=[];
+
+// for(let age of ages){
+//     const obj1={age:age, name:"dRAGON"};
+//     results.push(obj1);
+// }
+// // console.log(results);
+
+
+//functions
+// const arrck =[10,23,33,75,3,4,55,29,66,49,7,6,66,6,666,0,98,808,209.8, 0.7, -3.4, 4**6];
+// console.log(arrck);
+// let inpnum=  +prompt("enter the no to be checked :")
+// console.log(displaySum(inpnum1, inpnum2));
+
+//const displaySum = function(num1, num2){
+//     return `Sum is ${num1+num2}`;
+// }
+
+//const isEven = function (number){
+//     if(number%2==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// // console.log(isEven(inpnum1));
+// console.log(firstLetterExtractor(inpstring));
+
+// function firstLetterExtractor(string){
+//     return string[0];
+// }
+// console.log(searchNum(inpnum));
+
+// const searchNum = function(number){
+//     for(let item in arrck){
+//         if(arrck[item]==number){
+//             return `number ${number} exists at position ${item}.`
+//         }
+//     }
+// }
+
+const myArray=[1,666,"jetta","turbin2v.0",56456,6575,777,0.99978567567567754756756764564554645654654];
+let [injetor_no,,model, ...restAttr]=myArray;
+console.log(injetor_no,model, restAttr);
+
+
+const obj1={name:"sayooj", occ:"itemp", age:23, mrdstats:false};
+
+const {name:nm,age:ag}=obj1;
+console.log(nm, ag);
+
+const ar1=[{name:"say",age:22,gender:"M"},
+            {name:"ksa",age:21,gender:"F"},
+]
+
+for(item of ar1){
+   var obj2=item
+}
+
+const addnum = (num1, num2) => {
+    console.log(num1+num2);
+}
+
+
+
+const insider = () =>{
+    console.log("hello we are inside insider");
+    console.log("calling insider1...");
+    const insider1 =(name1) =>{
+        return `My name is ${name1}`;
+    }
+    console.log(insider1("fghfdb"
+    ));
+}
+
+insider();
+
+var name1="hjvbjhbvhj"
+{
+    console.log(name1);
+    var name1="sayooj"
+    console.log(name1.toUpperCase());
+}
+
+console.log(name1);
